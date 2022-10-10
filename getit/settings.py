c311ca3f2cd5cb79a6de1734f4e2ef79811d762e
arthurfonsek <arthur.b.dafonseca@gmail.com>
2022-10-09 22:06:50 -0300

    postgres

--- getit/settings.py
+++ getit/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 """
 Django settings for getit project.
 
@@ -76,14 +78,11 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'getit',
-        'USER': 'getituser',
-        'PASSWORD': 'getitsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/getit?user=getituser&password=getitsenha',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 # Password validation
